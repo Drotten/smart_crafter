1e6dea648790c806735e7ec34aab5d6771aa9f3d
Mark Tibblin <mark.tibblin@gmail.com>
2016-06-18 14:41:24 +0200

    Now adds the players as they are added in the game, instead of when trying to get one the first time.

--- services/server/crafter_info/crafter_info_service.lua
+++ services/server/crafter_info/crafter_info_service.lua
@@ -2,13 +2,23 @@ local CrafterInfoService = class()
 
 function CrafterInfoService:initialize()
    self._crafter_infos = {}
+   radiant.events.listen(_radiant, 'radiant:player_kingdom_changed', self, self._on_player_kingdom_changed)
+end
+
+function CrafterInfoService:_on_player_kingdom_changed(args)
+   return self:add_crafter_info(args.player_id)
+end
+
+function CrafterInfoService:add_crafter_info(player_id)
+   local crafter_info = radiant.create_controller('smart_crafter:crafter_info_controller', player_id)
+   self._crafter_infos[player_id] = crafter_info
+   return crafter_info
 end
 
 function CrafterInfoService:get_crafter_info(player_id)
    local crafter_info = self._crafter_infos[player_id]
    if not crafter_info then
-      crafter_info = radiant.create_controller('smart_crafter:crafter_info_controller', player_id)
-      self._crafter_infos[player_id] = crafter_info
+      crafter_info = self:add_crafter_info(player_id)
    end
    return crafter_info
 end
